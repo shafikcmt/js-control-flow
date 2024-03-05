f992a4117b4e47b8140778aef9c40441fa918eb7
shafikcmt <mdshafiqulislam822@gmail.com>
2024-03-05 12:25:38 +0530

    Practice Problem solving using control flow check week day

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ function checkNumber(n){
 checkNumber(5);
 checkNumber(-5);
 checkNumber(0);
+document.write('<hr>');
+document.write('<h4>Write a JavaScript program that takes a day of the week as input (using numbers 1-7, where 1 represents Monday and 7 represents Sunday) and prints whether it is a weekday or a weekend.</h4>');
+
+function checkDay(day){
+    if(day >= 1 && day <=5){
+        document.write('This is: WeekDay' +'<br>');
+    }else if(day>=6 && day <= 7){
+        document.write('This is: WeekEnd' +'<br>');
+    }else{
+        document.write('This is: Invalid Day' +'<br>');
+    }
+}
+checkDay(2);
+checkDay(6);
+checkDay(8);
